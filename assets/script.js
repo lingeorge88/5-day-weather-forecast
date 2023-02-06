efed2a9ce1779b1ba01c2ebeb824cfa8047d4028
lingeorge88 <lingeorge04@gmail.com>
2023-02-06 00:00:17 -0800

    added comments for JAVASCRIPT

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 const key = "0a4b5f5b92215551fbb579c3047f38a8";
-    var inputEl = document.getElementById("cityInput");
+    // selecting all DOM elements for manipulation later
+var inputEl = document.getElementById("cityInput");
     var searchBtn = document.getElementById("searchBtn");
     var clearEl = document.getElementById("clearHistory");
     var nameEl = document.getElementById("city-name");
@@ -14,7 +15,7 @@ const key = "0a4b5f5b92215551fbb579c3047f38a8";
 
 var lat;
 var lon;
-
+// using OpenWeatherMap Geomapping API to return the lat and lon values for a given city
     function getCoord(cityName){
         fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityName +"&limit=1&appid=" +key)
         .then(function(response){
@@ -32,6 +33,7 @@ var lon;
             fetch5day();
         })
     }
+    // fetches current Weather conditions from the API using lat and lon values obtained from geomapping api
     function fetchForecast(){
         fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+key)
         .then(function(response){
@@ -41,15 +43,15 @@ var lon;
         })
         .then(function(data){
             console.log(data);
+            // converting Unix time stamp into standard format
         const currentDate = new Date(data.list[0].dt*1000);
         console.log(currentDate);
         const day = currentDate.getDate();
         const month = currentDate.getMonth() + 1;
         const year = currentDate.getFullYear();
         nameEl.innerHTML= data.city.name + ", " + month + "/" + day + "/" + year;
-        console.log(month);
-            var weatherIcon = data.list[0].weather[0].icon;
-        // console.log(weatherIcon);
+        // filling out containers with information from the API
+        var weatherIcon = data.list[0].weather[0].icon;
         currentIconEl.setAttribute("src","https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
         currentWeather.innerHTML = data.list[0].weather[0].description;
         currentTemp.innerHTML="Temperature: " + k2C(data.list[0].main.temp) + "°C";
@@ -57,6 +59,7 @@ var lon;
         currentWind.innerHTML="Wind speed: " + data.list[0].wind.speed + " mph";
     })
 }
+// fetches 5 day forecast with lon and lat values obtained from geomapping api
 function fetch5day(){
     fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+key)
         .then(function(response){
@@ -67,6 +70,7 @@ function fetch5day(){
         .then(function(data){
             console.log(data);
         const forecastEls = document.querySelectorAll(".forecast");
+        // creating and appending elements from the api and filling out the 5 day forecast container
         for(var i = 0; i<forecastEls.length; i++){
             forecastEls[i].innerHTML="";
             const forecastIndex = i*8 +4;
@@ -93,7 +97,7 @@ function fetch5day(){
         }
 })
 }
-
+// When the user clicks the search button, we are saving the input to local storage unless it already exists, and passing the input through the getCoord function which will help us get the forecast
 searchBtn.addEventListener("click",function(){
     var searchName = inputEl.value;
     getCoord(searchName);
@@ -103,15 +107,17 @@ searchBtn.addEventListener("click",function(){
     }
     displaySearchHistory();
 })
+// clears the local storage and previous search results when clear history is clicked
 clearEl.addEventListener("click", function(){
     searchHistory=[];
     localStorage.clear();
     displaySearchHistory();
 })
+// converts default temperature in Kelvin to celsius
 function k2C(K){
     return Math.floor((K - 273.15));
 }
-
+// adding a input element for each previous search result to our container from the searchHistory array which comes from local storage
 function displaySearchHistory(){
     history1.innerHTML="";
     searchHistory.forEach(function(currentValue, index){
@@ -122,10 +128,7 @@ function displaySearchHistory(){
         prevSearch.setAttribute("class", "form-control d-block bg-black text-warning");
         prevSearch.setAttribute("id", "pointer");
         prevSearch.setAttribute("value", currentValue);
-        // prevSearch.textContent = currentValue;
-        // prevSearch.setAttribute("class", "btn btn-dark text-warning mt-1");
-        // prevSearch.setAttribute("type", "button");
-        // prevSearch.setAttribute("style", "display: block;");
+       
         prevSearch.addEventListener("click", function() {
             console.log(index);
             console.log(currentValue);
@@ -134,6 +137,7 @@ function displaySearchHistory(){
         history1.append(prevSearch);
     })
 }
+// displays users' last search result upon loading the page, if no previous searches exist then the page will be empty.
 function displayLastsearch(){
     if(searchHistory.length >  0){
         getCoord(searchHistory[searchHistory.length - 1]);
